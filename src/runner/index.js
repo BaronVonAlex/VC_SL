49ef1423125d2cd0a2e9012558702710193bbe83
BaronVonAlex <aleko.kotliarovi@gmail.com>
2024-03-14 00:24:50 +0400

    update: added error handler

--- src/runner/index.js
+++ src/runner/index.js
@@ -3,6 +3,7 @@ const { Client, IntentsBitField } = require('discord.js');
 const { CommandHandler } = require('djs-commander');
 const path = require('path');
 const { pullChanges } = require('../github/github');
+const { loadError } = require('../handler/errorHandler')
 
 const client = new Client({
   intents: [IntentsBitField.Flags.Guilds],
@@ -20,4 +21,4 @@ client.once('ready', async () => {
   await pullChanges();
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN).then(() => {loadError(client);}).catch((e) => console.log(e));

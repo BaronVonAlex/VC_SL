eeb2684d889eaf5cd014396497acbbb6587db007
BaronVonAlex <aleko.kotliarovi@gmail.com>
2024-10-27 20:56:14 +0400

    added new emoji for embed fields

--- src/util/embedFields.js
+++ src/util/embedFields.js
@@ -4,26 +4,26 @@ const getPlayerStat = (data, statName, defaultValue = 0) => {
 
 function generatePlayerFields(playerData, baseAttackStats, baseDefenceStats, fleetStats, playingSince, lastSeen, formattedUsernameHistory) {
     return [
-        { name: ':identification_card: Player ID', value: String(getPlayerStat(playerData, 'playerId')), inline: true },
-        { name: ':coin: Player Name', value: String(playerData.alias), inline: true },
-        { name: ':page_with_curl: Previous Names', value: formattedUsernameHistory, inline: false },
-        { name: ':beginner: Level', value: String(getPlayerStat(playerData, 'level')), inline: true },
-        { name: ':medal: Medals', value: String(getPlayerStat(playerData, 'medals')), inline: true },
+        { name: '<:CargoLedgers:1300126103998042185> Player ID', value: String(getPlayerStat(playerData, 'playerId')), inline: true },
+        { name: '<:VCIcon:1300134586717569149> Player Name', value: String(playerData.alias), inline: true },
+        { name: '<:Intel:1300137270950625392> Previous Names', value: formattedUsernameHistory, inline: false },
+        { name: '<:ModuleUpgrade:1300128880459517992> Level', value: String(getPlayerStat(playerData, 'level')), inline: true },
+        { name: '<:SolarShardsGold:1300134738736054363> Medals', value: String(getPlayerStat(playerData, 'medals')), inline: true },
         { name: ':ringed_planet: Planet', value: String(getPlayerStat(playerData, 'planet')), inline: true },
-        { name: ':desktop: Playing Since', value: playingSince, inline: true },
-        { name: ':hourglass: Last Seen', value: lastSeen, inline: true },
+        { name: '<:Time:1300129476780363908> Playing Since', value: playingSince, inline: true },
+        { name: '<:TimeTokens:1300134836425719889> Last Seen', value: lastSeen, inline: true },
         {
-            name: `:firecracker: Base Attack: ${baseAttackStats.totalBattles}, ${baseAttackStats.winratePercent}%, ${String(baseAttackStats.kdRatio)} K/D`,
+            name: `<:CollateralStat:1300135586602025062> Base Attack: ${baseAttackStats.totalBattles}, ${baseAttackStats.winratePercent}%, ${String(baseAttackStats.kdRatio)} K/D`,
             value: `Win: ${String(getPlayerStat(playerData, 'baseAttackWin'))}, Draws: ${String(getPlayerStat(playerData, 'baseAttackDraw'))}, Loss: ${String(getPlayerStat(playerData, 'baseAttackLoss'))}`,
             inline: false
         },
         {
-            name: `:shield: Base Defense: ${baseDefenceStats.totalBattles}, ${baseDefenceStats.winratePercent}%, ${String(baseDefenceStats.kdRatio)} K/D`,
+            name: `<:Modules_1:1300138611441795072> Base Defense: ${baseDefenceStats.totalBattles}, ${baseDefenceStats.winratePercent}%, ${String(baseDefenceStats.kdRatio)} K/D`,
             value: `Win: ${String(getPlayerStat(playerData, 'baseDefenceWin'))}, Draws: ${String(getPlayerStat(playerData, 'baseDefenceDraw'))}, Loss: ${String(getPlayerStat(playerData, 'baseDefenceLoss'))}`,
             inline: false
         },
         {
-            name: `:crossed_swords: Fleet vs Fleet: ${fleetStats.totalBattles}, ${fleetStats.winratePercent}%, ${String(fleetStats.kdRatio)} K/D`,
+            name: `<:WarFleet5:1300134200556523611> Fleet vs Fleet: ${fleetStats.totalBattles}, ${fleetStats.winratePercent}%, ${String(fleetStats.kdRatio)} K/D`,
             value: `Win: ${String(getPlayerStat(playerData, 'fleetWin'))}, Draws: ${String(getPlayerStat(playerData, 'fleetDraw'))}, Loss: ${String(getPlayerStat(playerData, 'fleetLoss'))}`,
             inline: false
         }

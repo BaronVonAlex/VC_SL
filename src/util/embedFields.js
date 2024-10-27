bf26367ef0de5fb3a579fda7912014703e938b7a
BaronVonAlex <aleko.kotliarovi@gmail.com>
2024-10-27 19:29:50 +0400

    moved previous name field inline with player ID

--- src/util/embedFields.js
+++ src/util/embedFields.js
@@ -5,8 +5,8 @@ const getPlayerStat = (data, statName, defaultValue = 0) => {
 function generatePlayerFields(playerData, baseAttackStats, baseDefenceStats, fleetStats, playingSince, lastSeen, formattedUsernameHistory) {
     return [
         { name: ':identification_card: Player ID', value: String(getPlayerStat(playerData, 'playerId')), inline: true },
-        { name: ':coin: Player Name', value: String(playerData.alias), inline: false },
-        { name: ':page_with_curl: Previous Names', value: formattedUsernameHistory },
+        { name: ':coin: Player Name', value: String(playerData.alias), inline: true },
+        { name: ':page_with_curl: Previous Names', value: formattedUsernameHistory, inline: false },
         { name: ':beginner: Level', value: String(getPlayerStat(playerData, 'level')), inline: true },
         { name: ':medal: Medals', value: String(getPlayerStat(playerData, 'medals')), inline: true },
         { name: ':ringed_planet: Planet', value: String(getPlayerStat(playerData, 'planet')), inline: true },
